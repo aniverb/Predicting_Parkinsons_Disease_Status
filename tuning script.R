44777796f0c18df2d03f950a03c27e527f50aba1
Tobi Bosede <abosede2@jhu.edu>
2016-12-12 12:26:04 -0500

    cv code added

--- tuning script.R
+++ tuning script.R
@@ -1,7 +1,8 @@
 #### project data
 #source("C:/Users/Tri/Documents/GitHub/Predicting_Parkinsons_Disease_Status/RFtesting.R")
 source("C:\\Users\\aniverb\\Documents\\Grad_School\\JHU\\475\\project\\Predicting_Parkinsons_Disease_Status\\RFtesting.R")
-setwd("C:/Users/Tri/Documents")
+#setwd("C:/Users/Tri/Documents")
+setwd("C:\\Users\\aniverb\\Documents\\Grad_School\\JHU\\475\\project\\Parkinsons data\\5 tests")
 library(randomForest)
 
 ## classic iris example
@@ -48,9 +49,10 @@ accuracy(iris, "Species", fp)
 irisRF <- buildForest(10, iris, "Species", info_gain = 0.1,
                       numOfFeatures = 4)
 compImpo(irisRF)
-##################################
 
-setwd("C:\\Users\\aniverb\\Documents\\Grad_School\\JHU\\475\\project\\Parkinsons data\\5 tests")
+##################################
+#our data
+##################################
 dt <- read.csv("roch_all_data.csv")
 
 ## Data cleaning
@@ -62,27 +64,31 @@ delete_index <- na_find(dt[3:(ncol(dt)-1)])
 if (length(delete_index) > 0) dt <- dt[-delete_index,]
 dt <- dt[-c(1,  ncol(dt))] ## Exclude file name and other useless information
 
+#to avoid java related rounding errors
 ddt <- apply(dt, 2, scale)
 ddt <- apply(dt, 2, round, digits = 5)
 ddt[, 1] <- dt[, 1]
 dt <- ddt
 dt <- as.data.frame(dt)
 
+#spliting data into train, dev, and test sets
 set.seed(12-4-16)
 train_index <- createDataPartition(1:nrow(dt), 0.5)[[1]]
 other <- seq(1, nrow(dt))[-train_index]
-test_index <- createDataPartition(1:length(other), 0.3)[[1]]
-dev_index <- seq(1, length(test_index))[-test_index]
+test_index <- sample(other, length(other) * 0.6)
+dev_index <- other[(other %in% test_index) == FALSE]
+#test_index <- createDataPartition(1:length(other), 0.3)[[1]]
+#dev_index <- seq(1, length(test_index))[-test_index]
 
 train <- dt[train_index, ]
-dev <- dt[dev_index, ] #alternative to cv
+dev <- dt[dev_index, ] #alternative to cross valid
 test <- dt[test_index, ]
 label <- "Status"
 
 #dt <- dt[train_index, c(1,seq(33, 186))]
 a <- Sys.time()
 set.seed(12-4-16)
-tuneForest=buildForest(20, train, label, max_depth = 8, info_gain = 0.001)#19 feat rule of thumb, depth is another param to tune 15 too high, 5 too low
+tuneForest=buildForest(10, train, label, info_gain = 0.001)#19 feat rule of thumb, depth is another param to tune 15 too high, 5 too low
 #Tree 11
 # Error in .jcall("weka/filters/Filter", "Lweka/core/Instances;", "useFilter",  : 
 #   java.lang.IllegalArgumentException: A nominal attribute (x_mean.Gait) cannot have duplicate labels ('(-0--0]').
@@ -92,7 +98,7 @@ tuneForest=buildForest(20, train, label, max_depth = 8, info_gain = 0.001)#19 fe
 b <- Sys.time()
 tuneFp=forestPredict(dev, tuneForest)
 c <- Sys.time()
-tuneAcc=accuracy(dev, label, tuneFp) #0.5906702, .51 with info gain.0001, tree size .1
+tuneAcc=accuracy(dev, label, tuneFp) # 0.5795007
 bf_time=b-a# 6.924353
 fp_time=c-b
 # tree #: 2 
@@ -101,8 +107,13 @@ fp_time=c-b
 #                   length of 'dimnames' [2] not equal to array extent
 compImpo(tuneForest)[1:10,]
 
-#tuning with package
+#CV/tuning with package
 a <- Sys.time()
+set.seed(12-4-16)
+rfCV=rfcv(dev[,2:ncol(dev)], factor(dev$Status), step=1, log=F, cv.fold=10, ntree=500)
+b <- Sys.time()
+1-min(rfCV$error.cv)
+which(rfCV$error.cv==min(rfCV$error.cv))
 packageForest=randomForest(factor(Status)~., train, replace=TRUE, ntree=500, importance=TRUE) #no depth/maxnodes
 b <- Sys.time()
 summary(packageForest)
@@ -110,7 +121,9 @@ packagePredict=predict(packageForest, dev)
 c <- Sys.time()
 bf_time=b-a# 6.924353
 fp_time=c-b
-accuracy(dev, label, packagePredict)# 0.8140604
+accuracy(dev, label, packagePredict)# 0.614532
+packagePredictT=predict(packageForest, test)
+accuracy(test, label, packagePredictT)#.6040526
 
 #most imp vars
 imp=packageForest$importance
